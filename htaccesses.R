eda0448417128fe05e9fa40af324a5f91d0e54d3
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-11-03 09:10:24 +0100

    [feature] Parse htaccesses into lines and WordPress-managed blocks

--- htaccesses.R
+++ htaccesses.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(archive)
 
 archive_lines <- function(archive_filename) {
@@ -5,8 +6,22 @@ archive_lines <- function(archive_filename) {
         rowwise %>%
         reframe(archive_read(archive_filename, path) %>%
                 read_lines %>%
-                tibble(path=path, lines=.))
+                tibble(path=path, line=.))
 }
 
-htaccesses_tar <- archive_lines("htaccesses.tar")
+count_found <- function(n, line, ...) {
+  seps <- list(...)  # Collect arbitrary number of separators in a list
 
+  case_when(
+      # Loop over the list of separators, checking each one by position.
+      # For the triple-bang see: https://rlang.r-lib.org/reference/splice-operator.html
+      !!!purrr::imap(seps, ~ { line == .x ~ ifelse(n == .y - 1, .y, n)}),
+      TRUE ~ n)
+}
+
+htaccesses <- archive_lines("htaccesses.tar") %>%
+    mutate(.by = path,
+           wordpress_block_step = accumulate(
+               line, .init=0,
+               count_found, "# BEGIN WordPress", "# END WordPress") %>%
+               .[-1])
